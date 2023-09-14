52667dd21d92052f509eb45c9f941c20274a4daa
UnCause <uncausedisskous@gmail.com>
2023-09-15 00:22:25 +0300

    Some updates

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //Selectors and variables
 let showForm = false;
+let notes = [{name: "kek", created: "Septemder 14, 2023", category: "Task", content: "some content", dates: "1/10/2024", isArchived: false, index: 0}];
 var btn_Show_create_form = document.querySelector(".show-create-form");
 var Note_Form_div = document.querySelector(".create-note-form");
 var Notes_table_body = document.querySelector(".notes-table-body");
 //Event Listeners
 btn_Show_create_form.addEventListener('click', showCreateForm);
+Notes_table_body.addEventListener('click', NoteClick);
+// Notes_table_body.addEventListener('beforeprint', loadNotes);
+window.onload = loadNotes;
+
 // btn_note_form_submit.addEventListener('click', addNote);
 
 //to export in external file
@@ -50,16 +55,28 @@ function showCreateForm (Event) {
     }
     
 }
-
-function addNote(Event) {
-    Event.preventDefault();
+function addNote (e) {
+    e.preventDefault();
+    console.log();
+    var Ncategory = document.addNote.Category.value;
+    var Nname = document.addNote.Name.value;
+    var Ncontent = document.addNote.Content.value;
+    var date = new Date();
+    var createdDate = months[date.getMonth()] + " " + date.getDate()  + 
+    ", " + date.getFullYear();
+    var Ndates = "to implement"
+    notes.push({name: Nname, created: createdDate, category: Ncategory, content: Ncontent, dates: Ndates, isArchived: false, index: notes.length});
+    console.log(notes[notes.length-1]);
+    createNote(notes[notes.length-1])
+}
+function createNote(note) {
+    // Event.preventDefault();
     var note_tr = document.createElement("tr");
     Notes_table_body.appendChild(note_tr);
-    var Ncategory = document.addNote.Category.value;
     var td_category = document.createElement("td");
-    td_category.innerText = Ncategory;
+    td_category.innerText = note.category;
     var td_icon = document.createElement("td");
-    switch (Ncategory) {
+    switch (note.category) {
         case "Task":
             td_icon.innerHTML = '<i class="fa-solid fa-paperclip"></i>';
             break;
@@ -69,18 +86,14 @@ function addNote(Event) {
         case "Random Thought":
             td_icon.innerHTML = '<i class="fa-solid fa-dice-d20"></i>';
     }
-    var Nname = document.addNote.Name.value;
     var td_name = document.createElement("td");
-    td_name.innerText = Nname;
+    td_name.innerText = note.name;
     var td_created = document.createElement("td");
-    var date = new Date();
-    td_created.innerText = months[date.getMonth()] + " " + date.getDate()  + 
-    ", " + date.getFullYear();
-    var Ncontent = document.addNote.Content.value;
+    td_created.innerText = note.createdDate;
     var td_content = document.createElement("td");
-    td_content.innerText = Ncontent;
+    td_content.innerText = note.content;
     var td_dates = document.createElement("td");
-    
+    td_dates.innerText = note.dates;
     var td_buttons = document.createElement("td");
     td_buttons.innerHTML = `
     <i class="fa-solid btn-archive fa-box-archive"></i>
@@ -92,19 +105,36 @@ function addNote(Event) {
     note_tr.appendChild(td_created);
     note_tr.appendChild(td_category);
     note_tr.appendChild(td_content);
+    note_tr.appendChild(td_dates);
     note_tr.appendChild(td_buttons);
-    var btn_archive = document.querySelector(".btn-archive");
-    var btn_delete = document.querySelector(".btn-delete");
-    var btn_edit = document.querySelector(".btn-edit");
-    btn_archive.addEventListener('click', ArchiveNote);
-    btn_delete.addEventListener('click', DeleteNote);
-    btn_edit.addEventListener('click', EditNote);
+}
+
+function NoteClick (e) {
+    item = e.target;
+
+    switch (item.classList[1]) {
+        case "btn-archive":
+            ArchiveNote(e);
+            break;
+        case "btn-delete":
+            DeleteNote(e);
+            break;
+        case "btn-edit":
+            EditNote(e);
+            break;
+        default:
+            break;
+    }
 }
 
 function getNote (node) {
     var btns = node.target.parentNode;
     var note = btns.parentNode;
     return note;
+}
+
+function loadNotes () {
+    alert("kek");
 }
 
 function ArchiveNote (e) {
@@ -115,6 +145,7 @@ function ArchiveNote (e) {
 function DeleteNote (e) {
     var note = getNote(e);
     Notes_table_body.removeChild(note);
+    // console.log(e.target.classList);
 }
 
 function EditNote (e) {
@@ -131,7 +162,7 @@ function EditNote (e) {
 
 
 //Just a task to check is a variable is an array, puttig out the numbers, 
-// deleting first and last element and summing the rest of the elements
+// deleting first and last element and summing the rest of the elements.
 
 // let arr = [7, 2, null, undefined, true, 1, 8, 10, "asd", {Age: 4}, [1, 4, 21]];
 // // let arr = 5;
